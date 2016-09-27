6b71e1a99e7639ae8e70eca262494ac5bbb95d39
yltwust <510212753@qq.com>
2016-09-27 17:54:58 +0800

    格式化代码

--- src/org/zywx/wbpalmstar/plugin/uexsegmentcontrol/ESegmentControlUtils.java
+++ src/org/zywx/wbpalmstar/plugin/uexsegmentcontrol/ESegmentControlUtils.java
@@ -19,11 +19,11 @@
 package org.zywx.wbpalmstar.plugin.uexsegmentcontrol;
 
 public class ESegmentControlUtils {
-	
-	public final static String SEGMENTCONTROL_PARAMS_JSON_KEY_ON_RESULT_INDEX = "index";
+
+    public final static String SEGMENTCONTROL_PARAMS_JSON_KEY_ON_RESULT_INDEX = "index";
     public final static String SEGMENTCONTROL_PARAMS_JSON_KEY_ON_RESULT_NAME = "name";
     public final static String SEGMENTCONTROL_PARAMS_JSON_KEY_ON_RESULT_SHOWS = "shows";
-    
+
     public static final String SEGMENTCONTROL_CALLBAKC_ON_ITEM_CLICK = "uexSegmentControl.onItemClick";
     public static final String SEGMENTCONTROL_CALLBACK_ON_DATA_CHANGE = "uexSegmentControl.onDataChange";
 }
--- src/org/zywx/wbpalmstar/plugin/uexsegmentcontrol/GridViewAdapter.java
+++ src/org/zywx/wbpalmstar/plugin/uexsegmentcontrol/GridViewAdapter.java
@@ -32,91 +32,91 @@ import java.util.List;
 
 public class GridViewAdapter extends BaseAdapter {
 
-	private Context context;
-	private EUExSegmentControl activity;
-	private List<String> titles;
-	private boolean edit;
+    private Context context;
+    private EUExSegmentControl activity;
+    private List<String> titles;
+    private boolean edit;
 
-	public GridViewAdapter(Context context, EUExSegmentControl activity,
-			List<String> titles) {
-		this.context = context;
-		this.activity = activity;
-		this.titles = titles;
-		edit = false;
-	}
+    public GridViewAdapter(Context context, EUExSegmentControl activity,
+                           List<String> titles) {
+        this.context = context;
+        this.activity = activity;
+        this.titles = titles;
+        edit = false;
+    }
 
-	public void setEdit(boolean edit) {
-		this.edit = edit;
-		notifyDataSetInvalidated();
-	}
+    public void setEdit(boolean edit) {
+        this.edit = edit;
+        notifyDataSetInvalidated();
+    }
 
-	@Override
-	public int getCount() {
-		return titles.size();
-	}
+    @Override
+    public int getCount() {
+        return titles.size();
+    }
 
-	@Override
-	public Object getItem(int position) {
-		return titles.get(position);
-	}
+    @Override
+    public Object getItem(int position) {
+        return titles.get(position);
+    }
 
-	@Override
-	public long getItemId(int position) {
-		return position;
-	}
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
 
-	@Override
-	public View getView(final int position, View convertView, ViewGroup parent) {
-		View view = null;
-		Holder holder = null;
-		if (convertView == null) {
-			holder = new Holder();
-			view = View.inflate(context, EUExUtil.getResLayoutID("plugin_uexsegmentcontrol_item_edit"), null);
-			holder.xx = (ImageView) view.findViewById(EUExUtil.getResIdID("plugin_uexsegmentcontrol_item_edit_iv"));
-			holder.tv = (TextView) view.findViewById(EUExUtil.getResIdID("plugin_uexsegmentcontrol_item_bt"));
-			view.setTag(holder);
-		} else {
-			view = convertView;
-			holder = (Holder) view.getTag();
-		}
-		if (edit) {
-			if (position == 0) {
-				holder.xx.setVisibility(View.INVISIBLE);
-			} else {
-				holder.xx.setVisibility(View.VISIBLE);
-			}
-		} else {
-			holder.xx.setVisibility(View.INVISIBLE);
-		}
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        View view = null;
+        Holder holder = null;
+        if (convertView == null) {
+            holder = new Holder();
+            view = View.inflate(context, EUExUtil.getResLayoutID("plugin_uexsegmentcontrol_item_edit"), null);
+            holder.xx = (ImageView) view.findViewById(EUExUtil.getResIdID("plugin_uexsegmentcontrol_item_edit_iv"));
+            holder.tv = (TextView) view.findViewById(EUExUtil.getResIdID("plugin_uexsegmentcontrol_item_bt"));
+            view.setTag(holder);
+        } else {
+            view = convertView;
+            holder = (Holder) view.getTag();
+        }
+        if (edit) {
+            if (position == 0) {
+                holder.xx.setVisibility(View.INVISIBLE);
+            } else {
+                holder.xx.setVisibility(View.VISIBLE);
+            }
+        } else {
+            holder.xx.setVisibility(View.INVISIBLE);
+        }
 
-		holder.tv.setText(titles.get(position));
-		holder.xx.setOnClickListener(new OnClickListener() {
+        holder.tv.setText(titles.get(position));
+        holder.xx.setOnClickListener(new OnClickListener() {
 
-			@Override
-			public void onClick(View v) {
-				String title = titles.remove(position);
-				notifyDataSetInvalidated();
-				activity.refreshView(title);
-			}
-		});
-		return view;
-	}
+            @Override
+            public void onClick(View v) {
+                String title = titles.remove(position);
+                notifyDataSetInvalidated();
+                activity.refreshView(title);
+            }
+        });
+        return view;
+    }
 
-	private class Holder {
-		public ImageView xx;
-		public TextView tv;
-	}
+    private class Holder {
+        public ImageView xx;
+        public TextView tv;
+    }
 
-	public void exchange(int dragPostion, int dropPostion) {
-		String dragItem = (String) getItem(dragPostion);
-		if (dragPostion < dropPostion) {
-			titles.add(dropPostion + 1, dragItem);
-			titles.remove(dragPostion);
-		} else {
-			titles.add(dropPostion, dragItem);
-			titles.remove(dragPostion + 1);
-		}
-		notifyDataSetChanged();
-	}
+    public void exchange(int dragPostion, int dropPostion) {
+        String dragItem = (String) getItem(dragPostion);
+        if (dragPostion < dropPostion) {
+            titles.add(dropPostion + 1, dragItem);
+            titles.remove(dragPostion);
+        } else {
+            titles.add(dropPostion, dragItem);
+            titles.remove(dragPostion + 1);
+        }
+        notifyDataSetChanged();
+    }
 
 }
